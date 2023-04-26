837917e579bafa892cf81483d6bacc2e04ff0eeb
William Castro <william.castromiller@outlook.com>
2023-04-26 14:10:05 -0400

    .

--- ProjetFinalWD4/Controllers/ReservationController1.cs
+++ ProjetFinalWD4/Controllers/ReservationController1.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetFinalWD4.Data;
 
 namespace ProjetFinalWD4.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly Bibliotheque _bibliotheque;
+        public ReservationController(Bibliotheque bibliotheque)
         {
-            return View();
+            _bibliotheque = bibliotheque;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var reservations = await _bibliotheque.Reservations.ToListAsync();
+            return View(reservations);
         }
     }
 }
